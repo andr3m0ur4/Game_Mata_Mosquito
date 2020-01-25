fd39941a5c0693b409d59622699d280e31d74c64
André Moura <andre.benedicto@etec.sp.gov.br>
2020-01-25 19:11:19 -0200

    Criando tamanho randômico para o elemento img

--- js/jogo.js
+++ js/jogo.js
@@ -23,7 +23,7 @@ function posicaoRandomica() {
 	// criar o elemento HTML
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
@@ -31,4 +31,18 @@ function posicaoRandomica() {
 	document.body.appendChild(mosquito)
 }
 
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	console.log(classe)
 
+	switch (classe) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
+}
