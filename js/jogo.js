910e2e25ab640db02ec9224ed795a458d8c11190
André Moura <andre.benedicto@etec.sp.gov.br>
2020-01-25 19:26:19 -0200

    Invertendo a imagem aleatoriamente na horizontal

--- js/jogo.js
+++ js/jogo.js
@@ -23,17 +23,17 @@ function posicaoRandomica() {
 	// criar o elemento HTML
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = tamanhoAleatorio()
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 
 	document.body.appendChild(mosquito)
+
 }
 
 function tamanhoAleatorio() {
 	var classe = Math.floor(Math.random() * 3)
-	console.log(classe)
 
 	switch (classe) {
 		case 0:
@@ -44,5 +44,18 @@ function tamanhoAleatorio() {
 
 		case 2:
 			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+
+	var classe = Math.floor(Math.random() * 2)
+
+	switch (classe) {
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
 	}
 }
