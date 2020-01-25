3cf5339cdf21ad2d3babf309b57a2e20bcc7b6c4
André Moura <andre.benedicto@etec.sp.gov.br>
2020-01-25 20:46:48 -0200

    Desenvolvendo a página de game over

--- js/jogo.js
+++ js/jogo.js
@@ -19,7 +19,7 @@ function posicaoRandomica() {
 
 		//console.log('Elemento selecionado foi: v' + vidas)
 		if (vidas > 3) {
-			alert('interromper o jogo (game over)')
+			window.location.href = 'fim_de_jogo.html'
 		} else {
 			document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
 
