d5fa82283b3f931b71654dbb6e4ea6f3e559527b
André Moura <andre.benedicto@etec.sp.gov.br>
2020-01-25 18:04:40 -0200

    Definindo a dimensão do palco do jogo

---
+++ js/jogo.js
@@ -0,0 +1,11 @@
+var altura = 0
+var largura = 0
+
+function ajustarTamanhoPalcoJogo () {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(altura, largura)
+}
+
+ajustarTamanhoPalcoJogo()
