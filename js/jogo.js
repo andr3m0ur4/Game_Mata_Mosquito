adb00983c05737fbea998a6dc52ff243c11af5f4
André Moura <andre.benedicto@etec.sp.gov.br>
2020-01-25 18:54:40 -0200

    Criando posições randômicas para o elemento img

--- js/jogo.js
+++ js/jogo.js
@@ -5,7 +5,30 @@ function ajustarTamanhoPalcoJogo () {
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 ajustarTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	// criar o elemento HTML
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+}
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName('body')[0].onresize = function() {
 	ajustarTamanhoPalcoJogo()
 }
+
+posicaoRandomica()
